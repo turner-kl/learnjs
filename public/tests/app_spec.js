9df432581e3b3fb1d0abe0590aae079468776268
Ryo Tanaka <ryo@RyonoiMac.local>
2020-02-16 23:39:01 +0900

    chapter3

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -1,3 +1,11 @@
+/***
+ * Excerpted from "Serverless Single Page Apps",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material,
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose.
+ * Visit http://www.pragmaticprogrammer.com/titles/brapps for more book information.
+***/
 describe('LearnJS', function () {
     it('can show a problem view', function () {
         learnjs.showView('#problem-1');
@@ -15,6 +23,12 @@ describe('LearnJS', function () {
         expect(learnjs.problemView).toHaveBeenCalledWith('42');
     });
 
+    it('triggers removingView event when removing the view', function () {
+        spyOn(learnjs, 'triggerEvent');
+        learnjs.showView('#problem-1');
+        expect(learnjs.triggerEvent).toHaveBeenCalledWith('removingView', []);
+    });
+
     it('invokes the router when loaded', function () {
         spyOn(learnjs, 'showView');
         learnjs.appOnReady();
@@ -26,6 +40,31 @@ describe('LearnJS', function () {
         spyOn(learnjs, 'showView');
         $(window).trigger('hashchange');
         expect(learnjs.showView).toHaveBeenCalledWith(window.location.hash);
+    });
+
+    it('can flash an element while setting the text', function () {
+        var elem = $('<p>');
+        spyOn(elem, 'fadeOut').and.callThrough();
+        spyOn(elem, 'fadeIn');
+        learnjs.flashElement(elem, "new text");
+        expect(elem.text()).toEqual("new text");
+        expect(elem.fadeOut).toHaveBeenCalled();
+        expect(elem.fadeIn).toHaveBeenCalled();
+    });
+
+    it('can redirect to the main view after the last problem is answered', function () {
+        var flash = learnjs.buildCorrectFlash(2);
+        expect(flash.find('a').attr('href')).toEqual("");
+        expect(flash.find('a').text()).toEqual("You're Finished!");
+    });
+
+    it('can trigger events on the view', function () {
+        callback = jasmine.createSpy('callback');
+        var div = $('<div>').bind('fooEvent', callback);
+        $('.view-container').append(div);
+        learnjs.triggerEvent('fooEvent', ['bar']);
+        expect(callback).toHaveBeenCalled();
+        expect(callback.calls.argsFor(0)[1]).toEqual('bar');
     });
 
     describe('problem view', function () {
@@ -43,20 +82,61 @@ describe('LearnJS', function () {
         });
 
         it('shows the problem code', function () {
-            expect(view.find('[data-name="code"]').text()).toEqual('function problem() {return __;}');
+            expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
+        });
+
+        describe('skip button', function () {
+            it('is added to the navbar when the view is added', function () {
+                expect($('.nav-list .skip-btn').length).toEqual(1);
+            });
+
+            it('is removed from the navbar when the view is removed', function () {
+                view.trigger('removingView');
+                expect($('.nav-list .skip-btn').length).toEqual(0);
+            });
+
+            it('contains a link to the next problem', function () {
+                expect($('.nav-list .skip-btn a').attr('href')).toEqual('#problem-2');
+            });
+
+            it('does not added when at the last problem', function () {
+                view.trigger('removingView');
+                view = learnjs.problemView('2');
+                expect($('.nav-list .skip-btn').length).toEqual(0);
+            });
         });
 
         describe('answer section', function () {
-            it('can check a correct answer by hitting a button', function () {
-                view.find('.answer').val('true');
-                view.find('.check-btn').click();
-                expect(view.find('.result').text()).toEqual('Correct!');
+            var resultFlash;
+
+            beforeEach(function () {
+                spyOn(learnjs, 'flashElement');
+                resultFlash = view.find('.result');
+            });
+
+            describe('when the answer is correct', function () {
+                beforeEach(function () {
+                    view.find('.answer').val('true');
+                    view.find('.check-btn').click();
+                });
+
+                it('flashes the result', function () {
+                    var flashArgs = learnjs.flashElement.calls.argsFor(0);
+                    expect(flashArgs[0]).toEqual(resultFlash);
+                    expect(flashArgs[1].find('span').text()).toEqual('Correct!');
+                });
+
+                it('shows a link to the next problem', function () {
+                    var link = learnjs.flashElement.calls.argsFor(0)[1].find('a');
+                    expect(link.text()).toEqual('Next Problem');
+                    expect(link.attr('href')).toEqual('#problem-2');
+                });
             });
 
             it('rejects an incorrect answer', function () {
                 view.find('.answer').val('false');
                 view.find('.check-btn').click();
-                expect(view.find('.result').text()).toEqual('Incorrect!');
+                expect(learnjs.flashElement).toHaveBeenCalledWith(resultFlash, 'Incorrect!');
             });
         });
     });
